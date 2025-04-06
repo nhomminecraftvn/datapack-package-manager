1970848cf0bad4423d0ef229b611a9664d83a6d8
BuildTools <unconfigured@null.spigotmc.org>
2025-04-06 12:09:35 +0700

    main

--- src/command/addFunction.js
+++ src/command/addFunction.js
@@ -15,13 +15,8 @@ module.exports.addFunction = function addFunction() {
         if (pack_formatRange < 3) throw `"pack_format: ${pack_formatRange}" datapack has not supported for Minecraft 1.12.2 (2017) or older`;
         else if (pack_formatRange >= 4 && pack_formatRange <= 48) cmdLib.addFunctions(pack_name, pack_data, function_name);
         else cmdLib.addFunction(pack_name, pack_data, function_name);
-    } else if (pack_name == undefined) {
-        throw `Missing <pack_name>`;
-    } else if (pack_data == undefined) {
-        throw `Missing <pack_data>`;
-    } else if (function_name == undefined) {
-        throw `Missing <function_name>`;
-    } else if (pack_formatRange == undefined) {
-        throw `Missing <pack_formatRange>`;
-    }
+    } else if (pack_name == undefined) throw `Missing <pack_name>`;
+      else if (pack_data == undefined) throw `Missing <pack_data>`;
+      else if (function_name == undefined) throw `Missing <function_name>`;
+      else if (pack_formatRange == undefined) throw `Missing <pack_formatRange>`;
 }
