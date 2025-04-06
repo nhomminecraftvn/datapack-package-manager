f407b7be26172120d40251be9ee09a5584c2ad03
NhomMinecraftVN <thachlong129@gmail.com>
2025-04-06 21:15:56 +0700

    main

--- src/command/init.js
+++ src/command/init.js
@@ -14,13 +14,13 @@ module.exports.package = function package() {
         pack_version = argv.slice(8);
 
     try {
-        if (pack_name && parseInt(pack_format) && description && pack_data && pack_version) cmdLib.initializePackage(pack_name, pack_format, description, pack_data, pack_author, pack_version);
+        if (pack_name && !isNaN(pack_format) && description && pack_data && pack_version) cmdLib.initializePackage(pack_name, pack_format, description, pack_data, pack_author, pack_version);
         else if (pack_name == undefined) throw `Missing <pack_name>`;
         else if (pack_format == undefined) throw `Missing <pack_format>`;
         else if (description == undefined) throw `Missing <description>`;
         else if (pack_data == undefined) throw `Missing <pack_data>`;
         else if (pack_version == undefined) throw `Missing <version>`;
-        else if (!parseInt(pack_format)) throw `"${pack_format}" it's not a number`;
+        else if (isNaN(pack_format)) throw `"${pack_format}" it's not a number`;
     } catch (expection) {
         console.log(expection)
     }
