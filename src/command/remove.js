0722f88c412348a4e6878a01002c043182bd5eac
NhomMinecraftVN <thachlong129@gmail.com>
2025-04-07 22:47:14 +0700

    main

---
+++ src/command/remove.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const Argv = require('../utils/argv');
+const CommandLibrary = require('../utils/commandLib');
+const argv = new Argv();
+var cmdLib = new CommandLibrary();
+
+module.exports.removePackagePackage = function removePackage() {
+    const pack_name = argv.slice(3)
+
+    try {
+        if (pack_name) {
+            cmdLib.removePackage(pack_name);
+        } else if (pack_name == undefined) throw `Missing <pack_name>`;
+    } catch (expection) {
+        console.log(expection)
+    }
+}
