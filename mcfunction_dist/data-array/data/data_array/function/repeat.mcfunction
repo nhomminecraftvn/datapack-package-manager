d1c1869ab0d04946cc25bf43e461be4d9ca54ade
NhomMinecraftVN <thachlong129@gmail.com>
2025-04-08 22:23:17 +0700

    mcfunction dist

---
+++ mcfunction_dist/data-array/data/data_array/function/repeat.mcfunction
@@ -0,0 +1,5 @@
+# index 7 arrays
+data modify storage number_arr main set value [1,2,3,4,5,6,7]
+
+# get
+execute store result storage number_arr main int 1 run scoreboard players get value array
---
+++ mcfunction_dist/data-array/dppm_package.js
@@ -0,0 +1,9 @@
+// This package information configure in package: data-array.
+var package = {
+    pack_name: "data-array",
+    pack_format: 61,
+    pack_description: "Data array",
+    pack_author: "nhomminecraftvn",
+    pack_version: "1.0.1",
+    created_at: "22:21:27 8/4/2025",
+}
